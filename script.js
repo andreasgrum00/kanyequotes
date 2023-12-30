7fb915519e6ff64cf7914b2e99c6fe491380b748
Andreas Grum <andreasgrum00@gmail.com>
2023-12-30 00:40:23 -0300

    added files

---
+++ script.js
@@ -0,0 +1,14 @@
+const imgElement = document.querySelector("img");
+const quoteElement = document.getElementById("quote");
+
+const imgs = ["img1.webp", "img2.webp", "img3.webp"];
+const img = imgs[Math.floor(Math.random() * imgs.length)];
+imgElement.src = `imgs/${img}`;
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://api.kanye.rest/")
+        .then(data => data.json())
+        .then(quote => {
+            quoteElement.textContent = `"${quote.quote}"`;
+        })
+})
